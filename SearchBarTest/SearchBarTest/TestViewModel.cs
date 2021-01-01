afffeccd183262799b2e67725c8af1c4e519b193
tragacheyskiy <de.stolyarov@gmail.com>
2021-01-01 16:35:21 +0500

    Initial

---
+++ SearchBarTest/SearchBarTest/TestViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SearchBarTest
+{
+    internal class TestViewModel
+    {
+        private bool isInitialized;
+
+        public Command TestCommand { get; }
+
+        public TestViewModel()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+
+                isInitialized = true;
+
+                Debug.WriteLine($">>> Initialized: {isInitialized}");
+
+                TestCommand.ChangeCanExecute();
+            });
+
+            TestCommand = new Command(() =>
+            {
+                Debug.WriteLine(">>> Command invoked.");
+            }, () => isInitialized);
+        }
+    }
+}
